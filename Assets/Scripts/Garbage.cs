b0a9daa0c469034e7b543de51087272643c3c21a
epainter <gavgavov@gmail.com>
2021-10-04 17:44:32 +0300

    Garbage falling

--- Assets/Scripts/Garbage.cs
+++ Assets/Scripts/Garbage.cs
@@ -5,10 +5,21 @@ using UnityEngine;
 
 public class Garbage : Particle
 {
-    [SerializeField] float disableCooldown = 1;
+    [SerializeField] float disableCooldown = 1.5f;
+    private Collider2D[] _collider2Ds;
+
+    protected override void Awake()
+    {
+        _collider2Ds = GetComponentsInChildren<Collider2D>();
+        base.Awake();
+    }
 
     protected override void OnEnable()
     {
+        foreach (var collider2D in _collider2Ds)
+        {
+            collider2D.enabled = true;
+        }
         StartCoroutine(DisableCooldown());
         base.OnEnable();
     }
@@ -17,5 +28,16 @@ public class Garbage : Particle
     {
         yield return new WaitForSeconds(disableCooldown);
         gameObject.SetActive(false);
+    }
+
+    private void OnCollisionEnter2D(Collision2D other)
+    {
+        if (other.gameObject.name == "GameField")
+        {
+            foreach (var collider2D in _collider2Ds)
+            {
+                collider2D.enabled = false;
+            }
+        }
     }
 }
